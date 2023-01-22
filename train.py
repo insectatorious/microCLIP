0f4525ca540a2345dd0d09ffef51a8093529a497
Sumanas Sarma <252960+insectatorious@users.noreply.github.com>
2023-01-22 10:01:53 +0000

    Use argparse to set vars

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 from datetime import datetime
 
 import numpy as np
@@ -9,17 +11,19 @@ from model import MicroCLIP
 from image_encoder.model import ResNet
 from text_transformer.model import TextTransformer
 
+# DATA_DIR = os.environ.get("DATA_DIR", '/Users/sumanas/Documents/data/cc3m')
 
-def main():
+
+def main(config):
   text_transformer = TextTransformer(64, num_heads=4, num_layers=4, ff_dim=128, num_classes=None)
   image_encoder = ResNet(filters=64, num_classes=None)
   image_encoder.build((None, 64, 64, 3))
 
   # Fetch test images from Imgur
   urls = [
-  "https://i.imgur.com/y8riKYY.jpg", # bus
-  "https://i.imgur.com/Tm45pBV.jpg", # cat
-  "https://i.imgur.com/86KDlmh.jpg", # dog
+    "https://i.imgur.com/y8riKYY.jpg",  # bus
+    "https://i.imgur.com/Tm45pBV.jpg",  # cat
+    "https://i.imgur.com/86KDlmh.jpg",  # dog
   ]
   images = [tf.keras.utils.get_file(origin=url) for url in urls]
   images = [tf.keras.preprocessing.image.load_img(image, target_size=(64, 64)) for image in images]
@@ -32,11 +36,11 @@ def main():
 
   clip = MicroCLIP(image_encoder=image_encoder,
                    text_encoder=text_transformer,
-                   temperature=10.1,
+                   temperature=config["temperature"],
                    tensorboard_dir=logdir)
-  dataset = load_cc3m('/Users/sumanas/Documents/data/cc3m',
+  dataset = load_cc3m(config["data_dir"],
                       text_transformer.tokenizer,
-                      batch_size=128)
+                      batch_size=config["batch_size"],)
 
   clip.compile(
     optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
@@ -44,7 +48,7 @@ def main():
   )
 
   clip.fit(dataset,
-           epochs=25,
+           epochs=config["epochs"],
            callbacks=[ImageTextCosineSimilarityCallback(images, texts_tokenised, logdir),
                       BatchMetricsCallback(logdir)])
   clip.save_weights('clip.h5')
@@ -53,4 +57,13 @@ def main():
 
 
 if __name__ == '__main__':
-  clip = main()
+  # Parse command line arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data_dir', required=True, type=str, help='Path to CC3M dataset')
+  parser.add_argument('--epochs', required=False, type=int, default=100, help='Number of epochs to train for')
+  parser.add_argument('--batch_size', required=False, type=int, default=128, help='Batch size')
+  parser.add_argument('--temperature', required=False, type=float, default=0.1, help='Temperature for softmax')
+
+  args = parser.parse_args()
+  config = vars(args)
+  clip = main(config)
